795da262141a348f7bc9160f3b07ed1c35dbf17e
finn <992639429@qq.com>
2020-10-25 15:39:48 +0800

    使用组件的render方法在React中显示输出

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    // return (
+    //   <div>
+    //     <h1>Hello World</h1>
+    //     <b>Bold</b>
+    //   </div>
+    // )
+    return <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
   }
 }
-
-// const App = () => (
-//   <h1>Hello World</h1>
-// )
 
 export default App
