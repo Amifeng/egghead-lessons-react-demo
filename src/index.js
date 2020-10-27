435cc566c8e0d65069edff18073a4b0b86b29534
finn <992639429@qq.com>
2020-10-27 17:36:09 +0800

    lesson-06: 使用Reacts props.children访问嵌套数据

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropsChildren from './lesson06/Props-children';
 // import App from './App'; //by lesson-03
 // import State from './lesson04/State'; //by lesson-04
-import State from './lesson05/State'; //by lesson-05
+// import State from './lesson05/State'; //by lesson-05
+import State from './lesson06/Props-children'; //by lesson-06
 
 ReactDOM.render(
   // <App txt="This is the prop txt" />,  //by lesson-03
-
-  <State />,  //by lesson-05
+  // <State />,  //by lesson-05
+  <PropsChildren />,  //by lesson-06
   
   document.getElementById('root')
 );
