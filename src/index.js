48f416ddcf9dbb03d4a0b9209bee00ad3829c892
finn <992639429@qq.com>
2020-10-28 21:57:47 +0800

    lesson-07 将自定义propType验证添加到React组件

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropsChildren from './lesson06/Props-children';
 // import App from './App'; //by lesson-03
 // import State from './lesson04/State'; //by lesson-04
 // import State from './lesson05/State'; //by lesson-05
-import State from './lesson06/Props-children'; //by lesson-06
+// import PropsChildren from './lesson06/Props-children'; //by lesson-06
+import CustomTitle from './lesson07/CustomTitle'; //by lesson-07
 
 ReactDOM.render(
   // <App txt="This is the prop txt" />,  //by lesson-03
   // <State />,  //by lesson-05
-  <PropsChildren />,  //by lesson-06
-  
+  // <PropsChildren />,  //by lesson-06
+  <CustomTitle />,  //by lesson-07
+
   document.getElementById('root')
 );
