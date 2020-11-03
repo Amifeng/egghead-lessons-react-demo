02f35da496ed61e01ef28db07d0be90b8d766d8b
finn <992639429@qq.com>
2020-11-03 21:42:54 +0800

    lesson-08 使用Reacts综合事件系统规范化事件

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 // import State from './lesson04/State'; //by lesson-04
 // import State from './lesson05/State'; //by lesson-05
 // import PropsChildren from './lesson06/Props-children'; //by lesson-06
-import CustomTitle from './lesson07/CustomTitle'; //by lesson-07
+// import CustomTitle from './lesson07/CustomTitle'; //by lesson-07
+import Events from './lesson08/Events'; //by lesson-08
 
 ReactDOM.render(
   // <App txt="This is the prop txt" />,  //by lesson-03
   // <State />,  //by lesson-05
   // <PropsChildren />,  //by lesson-06
-  <CustomTitle />,  //by lesson-07
+  // <CustomTitle />,  //by lesson-07
+  <Events />,  //by lesson-08
 
   document.getElementById('root')
 );
