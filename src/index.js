86c0b8a5b9f84f430968d837249f1d0f351a8a36
finn <992639429@qq.com>
2020-11-05 15:53:36 +0800

    lesson-09 使用React ref获取对特定组件的引用

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import ReactDOM from 'react-dom';
 // import State from './lesson05/State'; //by lesson-05
 // import PropsChildren from './lesson06/Props-children'; //by lesson-06
 // import CustomTitle from './lesson07/CustomTitle'; //by lesson-07
-import Events from './lesson08/Events'; //by lesson-08
+// import Events from './lesson08/Events'; //by lesson-08
+import Reference from './lesson09/Reference'
 
 ReactDOM.render(
   // <App txt="This is the prop txt" />,  //by lesson-03
-  // <State />,  //by lesson-05
+  // <State />,  //by lesson-04/ lesson-05
   // <PropsChildren />,  //by lesson-06
   // <CustomTitle />,  //by lesson-07
-  <Events />,  //by lesson-08
+  // <Events />,  //by lesson-08
+  <Reference />,  //by lesson-09
 
   document.getElementById('root')
 );
